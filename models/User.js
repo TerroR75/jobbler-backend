1c000ac0e8818fd35861230ad9e94131e5005961
TerroR75 <kamilsulimierski@gmail.com>
2023-03-20 17:40:32 +0100

    Removed old users.js and replaced it with User model
    Also removed /all endpoint to get all users from db

---
+++ models/User.js
@@ -0,0 +1,37 @@
+export default class User {
+  // Create a pointer to database on creation
+  constructor(db) {
+    this.db = db;
+  }
+
+  async create(name, email, password) {
+    const q = 'INSERT INTO users SET ?';
+    const user = { name, email, password };
+    const [result] = await this.db.query(q, user);
+    return result.insertId;
+  }
+
+  async findByEmail(email) {
+    const q = 'SELECT * FROM users WHERE email = ?';
+    const [rows] = await this.db.query(q, [email]);
+    return rows[0];
+  }
+
+  async findById(id) {
+    const q = 'SELECT * FROM users WHERE id = ?';
+    const [rows] = await this.db.query(q, [id]);
+    return rows[0];
+  }
+
+  async update(id, name, email, password) {
+    const q = 'UPDATE users SET name=?, email=?, password=? WHERE id=?';
+    const [result] = await this.db.query(q, [name, email, password, id]);
+    return result.affectedRows > 0;
+  }
+
+  async delete(id) {
+    const q = 'DELETE FROM users WHERE id = ?';
+    const [result] = await this.db.query(q, [id]);
+    return result.affectedRows > 0;
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,7 @@
 import express from 'express';
-import * as UserController from '../controllers/users.js';
 
 const router = express.Router();
 
-router.get('/all', async (req, res) => {
-  const users = await UserController.getAllUsers();
-  res.send(users);
-});
+router.get('/all', async (req, res) => {});
 
 export default router;
