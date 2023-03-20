c89cca1f4190b465798c206802c3783164c44c0e
TerroR75 <kamilsulimierski@gmail.com>
2023-03-20 17:47:06 +0100

    Signup validation middleware and User model implementation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,16 @@
 import db from '../config/database.js';
 import bcrypt from 'bcryptjs';
-import * as UserController from './users.js';
 import jwt from 'jsonwebtoken';
+
+// MODELS
+import User from '../models/User.js';
+const userM = new User(db); // Create a database connection to a User model
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-async function findByEmail(reqEmail) {
-  const q = 'SELECT * FROM users WHERE email = ?';
-  const [rows] = await db.query(q, [reqEmail]);
-  return rows[0];
-}
-
 export const register = async (req, res) => {
-  //    CHECK IF USER WITH THIS EMAIL ALREADY EXISTS
+  //    Input validated from middleware, now -> CHECK IF USER WITH THIS EMAIL ALREADY EXISTS
   const { name, email, emailC, password, passwordC } = req.body;
 
   // 1. First check if email and confirmation email do NOT match OR password and conf password do NOT match
@@ -24,8 +21,8 @@ export const register = async (req, res) => {
   } else {
     // 2. If email+emailC AND password+passwordC matches -> proceed with registration v
     // 3. Check if user with that email already exists
-    const user = await findByEmail(email);
-    if (user) {
+    const foundUser = await userM.findByEmail(email);
+    if (foundUser) {
       // 4. If so -> return
       res.status(400).send('Email is already used!');
       return;
@@ -33,7 +30,7 @@ export const register = async (req, res) => {
       // 5. If not -> hash password with bcrypt
       const salt = bcrypt.genSaltSync(10);
       const hashedPassword = bcrypt.hashSync(password, salt);
-      UserController.createUser(name, email, hashedPassword);
+      userM.create(name, email, hashedPassword);
       res.status(201);
       res.send('New user registered!');
     }
@@ -43,15 +40,15 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   // CHECK IF USER EXISTS
   const { email, password } = req.body;
-  const user = await findByEmail(email);
+  const foundUser = await userM.findByEmail(email);
 
   // 1. If user DOESNT exist -> return
-  if (!user) {
+  if (!foundUser) {
     res.send('Email or password is invalid.', 500);
     return;
   }
   // 2. If user DOES exist -> CHECK IF PASSWORD IS VALID
-  const isValid = await bcrypt.compare(password, user.password);
+  const isValid = await bcrypt.compare(password, foundUser.password);
   if (!isValid) {
     // 3. If password is invalid -> return
     res.send('Email or password is invalid.', 500);
@@ -62,10 +59,10 @@ export const login = async (req, res) => {
   // TODO SEND JWT
   const jwtToken = jwt.sign(
     {
-      id: user.id,
-      email: user.email,
+      id: foundUser.id,
+      email: foundUser.email,
     },
     process.env.JWT_SECRET
   );
-  res.json({ userId: user.id, name: user.name, token: jwtToken });
+  res.json({ userId: foundUser.id, name: foundUser.name, token: jwtToken });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import * as AuthController from '../controllers/auth.js';
+import * as Limiter from '../middleware/rateLimiter.js';
+import { signupValidation } from '../middleware/signupValidation.js';
 
 const router = express.Router();
 
-router.post('/register', AuthController.register);
+router.post('/register', Limiter.createAccountLimiter, signupValidation, AuthController.register);
 router.post('/login', AuthController.login);
 
 export default router;
